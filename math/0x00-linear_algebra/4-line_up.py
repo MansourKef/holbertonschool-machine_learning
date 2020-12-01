6eb6eba2a47444387c09c987f13b7f26174dbb33
MansourKef <2798@holbertonschool.com>
2020-12-01 17:12:58 +0000

    Add File 4

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def add_arrays(arr1, arr2):
         """
-    add_arrays
-    """
-    if len(arr1) != len(arr2):
-        return None
-    return [x + y for x, y in zip(arr1, arr2)]
+        add_arrays
+        """
+        if len(arr1) != len(arr2):
+                return None
+        return [x + y for x, y in zip(arr1, arr2)]
