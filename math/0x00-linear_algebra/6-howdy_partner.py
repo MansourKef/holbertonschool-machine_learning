acfb8362fa65e4b8f192fbabb2e064f07716448d
MansourKef <2798@holbertonschool.com>
2020-12-02 04:11:52 +0000

    Add File 6

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -6,8 +6,8 @@ cat_arrays
 
 
 def cat_arrays(arr1, arr2):
-        """
-        cat_arrays
-        """
-        arr3 = arr1 + arr2
-        return arr3
+    """
+    cat_arrays
+    """
+    arr3 = arr1 + arr2
+    return arr3
