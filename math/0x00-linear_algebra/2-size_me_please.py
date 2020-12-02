d7860c3e186cbf6294fab05d5b4d90de478e0d49
MansourKef <2798@holbertonschool.com>
2020-12-02 04:04:14 +0000

    Add File 2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -6,9 +6,9 @@ Module for matrix_shape.
 
 
 def matrix_shape(matrix):
-        """Finds the shape of matrix."""
-        dims = [len(matrix)]
-        while type(matrix[0]) != int:
-            dims.append(len(matrix[0]))
-            matrix = matrix[0]
-        return dims
+    """Finds the shape of matrix."""
+    dims = [len(matrix)]
+    while type(matrix[0]) != int:
+        dims.append(len(matrix[0]))
+        matrix = matrix[0]
+    return dims
