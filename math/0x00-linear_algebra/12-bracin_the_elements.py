bdd5b458190b8a1e79b23ce03db5620e5ef1ce67
MansourKef <2798@holbertonschool.com>
2020-12-02 06:04:35 +0000

    Add File 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+np_elementwise
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    np_elementwise
+    """
+    return mat1 + mat2, mat1 - mat2, mat1*mat2, mat1/mat2
