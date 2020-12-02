9d58cbbe7055fb62632495e029fb0934045f08b8
MansourKef <2798@holbertonschool.com>
2020-12-02 04:18:59 +0000

    Add File 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,10 +9,11 @@ def add_matrices2D(mat1, mat2):
     """
     add_matrices2D
     """
-    if len(mat1) != len(mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    result = [[0, 0],
-              [0, 0,]]
+
+    result = [[0 for i in mat1[0]] for j in mat1]
+    
     for i in range(len(mat1)):
         # iterate through columns
         for j in range(len(mat1[0])):
