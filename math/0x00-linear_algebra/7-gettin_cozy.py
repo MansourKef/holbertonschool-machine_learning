59d6b48db96be384499312931188d8cd59d7eacc
MansourKef <2798@holbertonschool.com>
2020-12-02 05:12:31 +0000

    Add File 7

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+cat_matrices2D
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    cat_matrices2D
+    """
+    mat3 = []
+    
+    if axis == 0:
+        return mat1 + mat2
+    if len(mat1) != len(mat2):
+        return None
+    for i in range(len(mat1)):
+        mat3.append(mat1[i] + mat2[i])
+    return mat3
