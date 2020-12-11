5d4fa5139e9c9af6e1b85374ac866722503896dc
MansourKef <2798@holbertonschool.com>
2020-12-11 09:28:47 +0000

    update File 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,9 +7,9 @@ def summation_i_squared(n):
     """
     9-summation_i_squared.py
     """
-    if type(n) != int:
+    if type(n) != int or n < 1:
         return None
     result = 0
     for i in range(1, n+1):
         result = result + i*i
-    return result
+    return (n * (n + 1) * (2 * n + 1)) // 6
