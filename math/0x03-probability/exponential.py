7320853ae1e66cb43658b1889bc666443b3e6f3d
MansourKef <2798@holbertonschool.com>
2020-12-15 23:51:50 +0000

    Apply expnential.py

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+exponential.py
+"""
+
+
+class Exponential:
+    """ 
+    Exponential
+    """
+    def __init__(self, data=None, lambtha=1.):
+        """
+        constructor
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = len(data) / sum(data)
+
+    def pdf(self, x):
+        """
+        pdf
+        """
+        if x < 0:
+            return 0
+        return ((self.lambtha) *
+                2818285 ** (-(self.lambtha) * x)))
+
+    def cdf(self, x):
+        """
+        cdf
+        """
+        if x < 0:
+            return 0
+        return 1 - (2.7182818285 **
+                    (-(self.lambtha * x)))
