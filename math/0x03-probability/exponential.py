d154dc190fe53e0aa18aa0dc9deb0340d6abfec2
MansourKef <2798@holbertonschool.com>
2020-12-16 00:04:41 +0000

    Apply expnential.py

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -31,3 +31,9 @@ class Exponential:
             return 0
         return ((self.lambtha) *
                 (2.7182818285 ** (-(self.lambtha) * x)))
+
+    def cdf(self, x):
+        """cdf"""
+        if x < 0:
+            return 0
+        return 1 - (2.7182818285 ** (-(self.lambtha * x)))
