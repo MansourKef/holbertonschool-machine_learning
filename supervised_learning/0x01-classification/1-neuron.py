2f84099a8da7f85fa3246a5672b73c76fbc4d693
MansourKef <2798@holbertonschool.com>
2021-01-02 09:21:10 +0000

    Add file 1

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+1-neuron.py
+"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron"""
+    def __init__(self, nx):
+        """Initializes the data."""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(loc=0.0, scale=1.0, size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def get__W(self):
+        """Get Function"""
+        return self.__W
+
+    @property
+    def get__b(self):
+        """Get Function"""
+        return self.__b
+
+    @property
+    def get__A(self):
+        """Get Function"""
+        return self.__A
