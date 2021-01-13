85f2aabfe5823db3f99ae3b5e1688af37fef4a72
MansourKef <2798@holbertonschool.com>
2021-01-13 18:35:25 +0000

    Add File 3

---
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""mod.
+"""
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """function
+    """
+    equal = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(equal, tf.float32))
+    return accuracy
