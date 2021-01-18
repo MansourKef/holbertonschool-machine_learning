5a0c4ec6959dfd28cea57903afa68bb3dcdd8018
MansourKef <2798@holbertonschool.com>
2021-01-18 18:42:45 +0000

    Add File 3

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+mod
+"""
+
+
+import numpy as np
+
+
+def specificity(confusion):
+    """function"""
+    diag = confusion.diagonal()
+    FN = np.sum(confusion, axis=1)
+    FP = np.add(np.sum(confusion, axis=0), - diag)
+    TN = np.sum(confusion) - FN - FP
+    return np.divide(TN, np.add(TN, FP))
