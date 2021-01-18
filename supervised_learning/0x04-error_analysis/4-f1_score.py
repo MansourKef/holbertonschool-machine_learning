4d11520236459379736471665d2985c0bcfa8b0b
MansourKef <2798@holbertonschool.com>
2021-01-18 18:45:01 +0000

    Add File 4

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+mod
+"""
+
+
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """
+    function
+    """
+    sens = sensitivity(confusion)
+    prec = precision(confusion)
+    mul = np.multiply(sens, prec)
+    add = np.add(sens, prec)
+    return 2 * np.divide(mul, add)
