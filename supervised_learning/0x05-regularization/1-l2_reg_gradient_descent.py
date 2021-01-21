a3020179f537a08b39603a890ade8f09d1ca0b2f
MansourKef <2798@holbertonschool.com>
2021-01-21 17:44:37 +0000

    Add File 1

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -6,20 +6,15 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """function"""
-     weights_t = weights.copy()
-     m = Y.shape[1]
-     for i in reversed(range(L)):
-         if i == L - 1:
-             dZ = cache['A{}'.format(i + 1)] - Y
-             dW = (np.matmul(dZ, cache['A{}'.format(i)].T)) / m
-         else:
-             dZa = np.matmul(weights_t['W{}'.format(i + 2)].T, dZ)
-             dZb = 1 - cache['A{}'.format(i + 1)]**2
-             dZ = dZa * dZb
-             dW = (np.matmul(dZ, cache['A{}'.format(i)].T)) / m
-        dW_reg = dW + (lambtha / m) * weights_t['W{}'.format(i + 1)]
-        db = np.sum(dZ, axis=1, keepdims=True) / m
-        weights['W{}'.format(i + 1)] = weights_t['W{}'.format(i + 1)] \
-            - (alpha * dW_reg)
-        weights['b{}'.format(i + 1)] = weights_t['b{}'.format(i + 1)] \
-            - (alpha * db)
+    weights_copy = weights.copy()
+    dz = cache["A" + str(L)] - Y
+    for i in range(L, 0, -1):
+        A = cache["A" + str(i - 1)]
+        w = "W" + str(i)
+        b = "b" + str(i)
+        dw = (1 / len(Y[0])) * np.matmul(dz, A.T) + 1/len(
+            Y[0]) * lambtha * weights[w]
+        db = (1 / len(Y[0])) * np.sum(dz, axis=1, keepdims=True)
+        weights[w] = weights[w] - alpha * dw
+        eights[b] = weights[b] - alpha * db
+        dz = np.matmul(weights_copy["W" + str(i)].T, dz) * (1 - A * A)
