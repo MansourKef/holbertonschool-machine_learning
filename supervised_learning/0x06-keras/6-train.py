04df9cab3a7e5e0fe9e789def318748956152e88
MansourKef <2798@holbertonschool.com>
2021-01-26 19:05:19 +0000

    Add

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """function"""
+    if early_stopping and validation_data is not None:
+        early_stop = K.callbacks.EarlyStopping(monitor="val_loss",
+                                               patience=patience)
+        callbacks = [early_stop]
+    else:
+        callbacks = None
+        history = network.fit(data, labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose,
+                              callbacks=callbacks, shuffle=shuffle,
+                              validation_data=validation_data)
+    return history
