7ffefc9e4543633da36b20de2c705f27d48dab20
MansourKef <2798@holbertonschool.com>
2021-01-26 19:00:42 +0000

    Add

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs, verbose=True,
+                shuffle=False):
+    """function"""
+    history = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=int(verbose),
+                          shuffle=shuffle)
+    return history
