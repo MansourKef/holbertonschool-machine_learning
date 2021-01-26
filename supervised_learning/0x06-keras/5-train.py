fa6fc176e299aa32460be3e908d5076cd055a107
MansourKef <2798@holbertonschool.com>
2021-01-26 19:03:21 +0000

    Add

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                verbose=True, shuffle=False):
+    """function"""
+    if not type(validation_data) is tuple:
+        validation_data = None
+        history = network.fit(data, labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose,
+                              shuffle=shuffle,
+                              validation_data=validation_data)
+    return history
