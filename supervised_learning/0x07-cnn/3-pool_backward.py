e1b001ffc94a05aa2f7084e2603c020c9018f3a1
MansourKef <2798@holbertonschool.com>
2021-02-04 14:26:14 +0000

    Add

---
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """function"""
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    h_new, w_new, c_new = dA.shape[1:]
+    dA_prev = np.zeros(A_prev.shape)
+    for n in range(m):
+        for k in range(c_new):
+            for i in range(h_new):
+                for j in range(w_new):
+                    if mode == 'max':
+                        A_prev_sliced = A_prev[n, i*sh: i*sh + kh,
+                                               j*sw: j*sw + kw, k]
+                        position = (A_prev_sliced == np.max(A_prev_sliced))
+                        dA_prev[n, i*sh: i*sh + kh, j*sw: j*sw + kw, k] = \
+                            position*dA[n, i, j, k]
+                    else:
+                        dA_prev[n, i*sh: i*sh + kh, j*sw: j*sw + kw, k] = \
+                            dA[n, i, j, k]/(kh*kw)
+    return dA_prev
