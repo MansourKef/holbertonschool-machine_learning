a0d592ced797f70e0e165d1e29361181bc1897bb
MansourKef <2798@holbertonschool.com>
2021-02-04 15:32:24 +0000

    Add

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -9,22 +9,25 @@ def conv_forward(A_prev, W, b, activation, padding="same",
                 stride=(1, 1)):
     """function"""
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
+    kh, kw = W.shape[0], W.shape[1]
+    c_new = W.shape[3]
     sh, sw = stride
-    pad_h, pad_w = (0, 0)
-    if padding == "same":
-        pad_h = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
-        pad_w = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
-    padded = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
-                    stant_values=(0, 0))
-    conv_h = (h_prev + 2 * pad_h - kh) // sh + 1
-    conv_w = (w_prev + 2 * pad_w - kw) // sw + 1
-    convolved = np.zeros((m, conv_h, conv_w, c_new))
-    for row in range(conv_h):
-        for col in range(conv_w):
-            for ch in range(c_new):
-                slice_A = padded[:, row * sh:row * sh + kh, col * sw:col * sw
-                                 + kw]
-                slice_A_sum = np.sum(slice_A * W[:, :, :, ch], axis=(1, 2, 3))
-                convolved[:, row, col, ch] = slice_A_sum
-    return activation(convolved + b)
+    if padding == 'valid':
+        h_new = int(np.ceil((h_prev - kh + 1)/sh))
+        w_new = int(np.ceil((w_prev - kw + 1)/sw))
+        A_padded = A_prev[:, :, :, :]
+    elif padding == 'same':
+        ph = int(np.ceil(((h_prev - 1)*sh + kh - h_prev)/2))
+        pw = int(np.ceil(((w_prev - 1)*sw + kw - w_prev)/2))
+        h_new = int(np.ceil(((h_prev + 2*ph - kh + 1)/sh)))
+        w_new = int(np.ceil(((w_prev + 2*pw - kw + 1)/sw)))
+        A_padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
+    output = np.zeros((m, h_new, w_new, c_new))
+    for k in range(c_new):
+        for i in range(h_new):
+            for j in range(w_new):
+                sliced = A_padded[:, i*sh: i*sh + kh, j*sw: j*sw + kw, :]
+                output[:, i, j, k] = np.sum(sliced*W[:, :, :, k],
+                                            axis=(1, 2, 3))
+    output = activation(output + b)
+    return output
